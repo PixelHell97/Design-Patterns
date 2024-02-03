949481edc98f9f945d309606dce7ff3450d19780
PixelHell97 <abakhet2010@gmail.com>
2024-02-03 20:09:13 +0200

    builder && factory

---
+++ src/design_patterns/Builder.kt
@@ -0,0 +1,62 @@
+package design_patterns
+
+
+
+class Pizza private constructor(pizzaBuilder: PizzaBuilder) {
+    private val size: String
+    private val cheese: Boolean
+    private val pepperoni: Boolean
+    private val mushrooms: Boolean
+    private val sausage: Boolean
+
+    init {
+        size = pizzaBuilder.size
+        cheese = pizzaBuilder.cheese
+        pepperoni = pizzaBuilder.pepperoni
+        mushrooms = pizzaBuilder.mushrooms
+        sausage = pizzaBuilder.sausage
+    }
+
+    override fun toString(): String {
+        return "Pizza(size='$size', cheese=$cheese, pepperoni=$pepperoni, mushrooms=$mushrooms, sausage=$sausage)"
+    }
+
+    class PizzaBuilder {
+        var size: String = ""
+            private set
+        var cheese: Boolean = false
+            private set
+        var pepperoni: Boolean = false
+            private set
+        var mushrooms: Boolean = false
+            private set
+        var sausage: Boolean = false
+            private set
+
+        fun setSize(size: String): PizzaBuilder = apply { this.size = size }
+
+        fun addCheese(): PizzaBuilder = apply { this.cheese = true }
+
+        fun addPepperoni(): PizzaBuilder = apply { this.pepperoni = true }
+
+        fun addMushrooms(): PizzaBuilder = apply { this.mushrooms = true }
+
+        fun addSausage(): PizzaBuilder = apply { this.sausage = true }
+
+        fun build(): Pizza {
+            return Pizza(this)
+        }
+    }
+}
+
+fun main() {
+    val pizza = Pizza.PizzaBuilder()
+        .setSize("Large")
+        .addCheese()
+        .addPepperoni()
+        .addMushrooms()
+        .build()
+
+
+    println(pizza.toString())
+}
---
+++ src/design_patterns/Factory.kt
@@ -0,0 +1,47 @@
+package design_patterns
+
+interface Shape {
+    fun draw()
+}
+
+
+class Triangle : Shape {
+    override fun draw() {
+        println("Drawing Triangle")
+    }
+}
+
+class Square : Shape {
+    override fun draw() {
+        println("Drawing Square")
+    }
+}
+
+class Pentagon : Shape {
+    override fun draw() {
+        println("Drawing Pentagon")
+    }
+}
+
+// Factory class
+object ShapeFactory {
+    fun getShape(numberOfRips: Int): Shape? {
+        return when (numberOfRips) {
+            3 -> Triangle()
+            4 -> Square()
+            5 -> Pentagon()
+            else -> null
+        }
+    }
+}
+
+fun main() {
+    val circle: Shape? = ShapeFactory.getShape(3)
+    circle?.draw()
+
+    val rectangle: Shape? = ShapeFactory.getShape(4)
+    rectangle?.draw()
+
+    val square: Shape? = ShapeFactory.getShape(5)
+    square?.draw()
+}
